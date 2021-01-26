a65492386dcef74241cc6225a7968292bd9e8298
Daniel <dbuehler1@my.wctc.edu>
2021-01-25 19:29:12 -0600

    commiting to github

--- githubProject/Program.cs
+++ githubProject/Program.cs
@@ -7,6 +7,7 @@ namespace githubProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Test");
         }
     }
 }
